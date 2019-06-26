053f13c13fdad54cfe45d203c3b658cf501f55b9
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-26 15:48:37 +0100

    Added function for confirming the ending

--- js/app.js
+++ js/app.js
@@ -3,8 +3,14 @@
 window.onload = function(event) {
 	document.getElementById("copyright-year").textContent = (new Date()).getFullYear();
 
+	function confirm(string, target) {
+		
+	}
+
 	let form = document.getElementById("form");
 	form.addEventListener("submit", function(event) {
 		event.preventDefault();
+
+		confirm(this.children[0].value, this.children[1].value);
 	});
 };
